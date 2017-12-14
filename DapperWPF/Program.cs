88d2e901d2948d6897bb908a76417641232a61f9
Jakub Marcickiewicz <jakub.marcickiewicz@plk-sa.pl>
2017-12-14 15:50:31 +0100

    Deleted usings

--- DapperWPF/Program.cs
+++ DapperWPF/Program.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Dapper;
-using DapperWPF.Models;
-using LiteDB;
-
 namespace DapperWPF
 {
     class Program
